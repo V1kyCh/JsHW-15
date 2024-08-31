b97e453b75beff048772391e2184295b61edfe3f
V1kyCh <126974500+V1kyCh@users.noreply.github.com>
2024-08-31 11:31:17 +0300

    all done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const users = [
         name: 'user2',
         balance: 6666,
         friends: ['Roxa', 'Roma'],
-        skills: ['boxing','taekwondo']
+        skills: ['boxing', 'taekwondo']
     },
     {
         name: 'user3',
@@ -20,19 +20,26 @@ const users = [
 ]
 
 // №1
-let sumBalance = 0;
-users.map(object => object.balance).forEach(balance => sumBalance += balance);
+const sumBalance = users.reduce((sum, obj) => sum + obj.balance, 0)
 console.log(sumBalance);
 
 // №2
 const chooseFriend = 'Max';
-const arrayNamesOfFriend = users.filter(object => object.friends.some(friendName => friendName === chooseFriend)).map(object => object.name);
-console.log(arrayNamesOfFriend);
+const arrayNamesOfFriends = users.reduce((arr, obj) => {
+    if (obj.friends.includes(chooseFriend)) {
+        arr.push(obj.name);
+    }
+    return arr
+}, [])
+console.log(arrayNamesOfFriends);
 
 // №3
 const sortFriends = [...users].sort((currentEl, nextEl) => currentEl.friends.length - nextEl.friends.length)
 console.log('Посортовано в порядку зростання', sortFriends);
 
 // №4
-const allSkills = [...users.map(object => object.skills).flat()].sort();
+const allSkills = [...users.reduce((arr, obj) => {
+    arr.push(obj.skills)
+    return arr
+}, []).flat()].sort()
 console.log(allSkills);
